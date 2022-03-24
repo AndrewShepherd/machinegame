cc045f006f9dcb510f32fb6cead7582dbda98745
Andrew Shepherd <shepherd_a@hotmail.com>
2022-03-24 15:41:13 +1000

    Refactoring the battle planner

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ machinegame - code to play
 To compete against this code, try: https://www.machinegame.com/vs/AndrewShepherd
 
 At the moment, this code wins 4/10 times against [acavailhez](https://www.machinegame.com/vs/acavailhez).
+
+It gets into a stalemat3 againt [manveru](https://www.machinegame.com/vs/manveru)
+He puts one guard on the flag
--- play.ts
+++ play.ts
@@ -325,12 +325,92 @@ function assignRobotsToLocations<T extends Point>(
   return assignmentCount;
 }
 
+namespace Battle {
+  interface EnemyNode {
+    enemy: EnemyRobot;
+    attackingRobots: Robot[];
+  }
+
+  interface Move {
+    robot: Robot;
+    location: Point;
+  }
+
+  interface Attack {
+    robot: Robot;
+    enemy: EnemyRobot;
+  }
+
+  export interface BattlePlan {
+    moves: Move[];
+    attacks: Attack[];
+  }
+  
+  export class BattlePlanner {
+
+    public constructor(private readonly state: GameState) {
+    }
+  
+    public generatePlan(): BattlePlan {
+      const nodes = this.state.red.robots.map(r => { return { enemy: r, attackingRobots:[] }});
+
+      const robotsAndNodes:{ robot: Robot, node: EnemyNode, proximity: number }[] = [];
+      for(let r of this.state.robots) {
+        for(let n of nodes) {
+          robotsAndNodes.push(
+            { 
+              robot: r,
+              node: n,
+              proximity: proximity(r, n.enemy)
+            }
+          );          
+        }
+      }
+      robotsAndNodes.sort(
+        (l, r) => l.proximity < r.proximity ? 1 : (l.proximity > r.proximity ? -1 : 0)
+      );
+      const plan:BattlePlan = {
+        moves: new Array<Move>(),
+        attacks: new Array<Attack>()
+      };
+      // Lots of improvements to make here.
+      // - If the flag has been found rush the flag
+      // - If the robot is two away from the enemy it should
+      //   not move in because of first strike
+      while(robotsAndNodes.length) {
+        const e = robotsAndNodes.pop();
+        if(e.proximity === 1) {
+          plan.attacks.push({ robot: e.robot, enemy: e.node.enemy});
+        } else {
+          plan.moves.push({ robot: e.robot, location: e.node.enemy});
+        }
+        e.node.attackingRobots.push(e.robot);
+        const enemyIsBeaten = e.node.enemy.charges <= e.node.attackingRobots.length;
+        const indexesToDelete: number[] = [];
+        for(let i = 0; i < robotsAndNodes.length; ++i) {
+          const rn = robotsAndNodes[i];
+          if( (rn.robot === e.robot) || (enemyIsBeaten && (rn.node === e.node))) {
+            indexesToDelete.push(i);
+          }
+        }
+        for(let i = indexesToDelete.length - 1; i >= 0; --i) {
+          robotsAndNodes.splice(i, 1);
+        }
+      }
+      return plan;
+    } 
+  }
+}
+
+
+
+
 function play(state : GameState) {
   ++iteration;
   const millisecondsStart = Date.now();
   console.log(`${iteration}: Starting iteration ${iteration}`);
   
-  const availableRobots = state.robots.filter(r => !!r);
+  let availableRobots:Robot[] = state.robots.filter(r => !!r);
   const countOfAllRobots = availableRobots.length;
   const locationsOccupied = [];
   for(let r of availableRobots) {
@@ -348,74 +428,23 @@ function play(state : GameState) {
 
    chargeRegister.incorporate(state.charges.filter(c => !!c));
 
-   let totalAttackers = 0;
-   if(state.red.robots.length) {
-     console.log(`${iteration}: Found enemy! ${state.red.robots.length} enemy robots`);
-    let robotsToAttack = state.red.robots.slice();
-    /*
-    // First of all, any robots with a proximity of 1
-    // Nested loop: BAD
-    const attackingRobots:Robot[] = [];
-    for(let r of availableRobots) {
-      let deadEnemy:EnemyRobot = undefined;
-      for(let e of robotsToAttack) {
-        if(proximity(r, e) <= 1) {
-          let eCharges = e.charges - 1;
-          r.attack(e);
-          if(eCharges == 0) {
-            deadEnemy = e;
-          }
-          attackingRobots.push(r);
-          break;
-        }
-      }
-      if(deadEnemy) {
-        robotsToAttack.splice(robotsToAttack.indexOf(deadEnemy), 1);
-      }
-    }
-    for(let r of attackingRobots) {
-      availableRobots.splice(availableRobots.indexOf(r), 1);
-    }
-    */
-    while(robotsToAttack.length && availableRobots.length) {
-      const enemyWithHealthRemaining = robotsToAttack.slice();
-      totalAttackers += assignRobotsToLocations(
-        availableRobots,
-        robotsToAttack,
-        (robot, enemy) => {
-          switch(proximity(robot, enemy)) {
-            case 1:
-              robot.attack(enemy);
-              break;
-            case 2:
-              // Just wait. We don't want enemy to have first strike
-              break;
-            default:
-              robot.moveTo(enemy);
-          }
-          enemy.charges -= robot.charges;
-          if(enemy.charges < 1) {
-            enemyWithHealthRemaining.splice(enemyWithHealthRemaining.indexOf(enemy), 1);
-          }
-          return true;
-        }
-      );
-      robotsToAttack = enemyWithHealthRemaining;
-    }
+
+   enemyFlag = enemyFlag || state.red.flag;
+  const battlePlanner = new Battle.BattlePlanner(state);
+  const plan = battlePlanner.generatePlan();
+  for(let a of plan.attacks) {
+    a.robot.attack(a.enemy);
+    availableRobots.splice(availableRobots.indexOf(a.robot), 1);
   }
-    console.log(`${iteration}: [${Date.now()-millisecondsStart}] Assigned ${totalAttackers} attackers`);
-
-
-
-   
-   
-   
-  enemyFlag = enemyFlag || state.red.flag;
+  for(let a of plan.moves) {
+    a.robot.moveTo(a.location);
+    availableRobots.splice(availableRobots.indexOf(a.robot), 1);
+  }
   if(enemyFlag) {
       console.log(`${iteration}: Found flag at (${enemyFlag.x}, ${enemyFlag.y})`)
-
+      const robotsTwoAway:Robot[] = [];
      for(let r of availableRobots) {
-       guardedMoveTo(r, enemyFlag);
+       r.moveTo(enemyFlag);
      }
      availableRobots.splice(0, availableRobots.length)
    }
